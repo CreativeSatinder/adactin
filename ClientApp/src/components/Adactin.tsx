59445cd1557a4efe296d2d7bb75cce90af242e8f
Satinder Singh <126883284+CreativeSatinder@users.noreply.github.com>
2023-03-04 15:44:32 -0500

    Add files via upload

--- ClientApp/src/components/Adactin.tsx
+++ ClientApp/src/components/Adactin.tsx
@@ -6,15 +6,24 @@ import {
     Label, FormGroup, Input, Spinner, Collapse
 } from 'reactstrap';
 import { Occupation, Rating } from '../models/Classes'
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css"; 
+import * as Helpers from '../helpers/DateTimeHelpers';
 
 interface PremiumCalculatorProps{
+
 }
 
 interface PremiumCalculatorState {
     selectedOccupation: string,
     selectedRatingID: number,
+    selectedFactor : number,
+    selectedSumInsured: number,
+    selectedName: string,
     occupations: Array<Occupation>,
     ratings: Array<Rating>,
+    calculatedAge: number,    
+    calculatedMonthlyPremium: number,
 }
 
 const ratinglist = [
@@ -25,13 +34,16 @@ const ratinglist = [
 ]
 
 const occupationlist =  [
+    {occupation:"-Select-", ratingid: 0},
     {occupation:"Cleaner", ratingid: 3},
     {occupation: "Doctor", ratingid: 1},
     {occupation: "Author", ratingid: 2},
     {occupation: "Farmer", ratingid: 4},
     {occupation: "Mechanic", ratingid: 4},
-    { occupation: "Florist", ratingid: 3}    
+    {occupation: "Florist", ratingid: 3}    
 ]
+
+const maxAge = 70;
 
 class PremiumCalculator extends React.Component<PremiumCalculatorProps, PremiumCalculatorState> {
     constructor(props: PremiumCalculatorProps) {
@@ -39,19 +51,90 @@ class PremiumCalculator extends React.Component<PremiumCalculatorProps, PremiumC
         this.state = {
             selectedOccupation:"",
             selectedRatingID: 0,
+            selectedFactor: 0,
+            selectedSumInsured: 0,
+            calculatedAge:0,
+            calculatedMonthlyPremium:0,
+            selectedName: "",                
             occupations: occupationlist,
-            ratings: ratinglist                
+            ratings: ratinglist,
         }
     }
 
-    handleOccupationChange = (occupation: Occupation) => {
-        alert(occupation.occupation);
+    handleOccupationChange = (event: any) => {
+            let occupationValue = occupationlist.filter(o => o.occupation == event.target.value)[0];
+            let ratingValue = ratinglist.filter(r => r.ratingid == occupationValue.ratingid)[0];
+            this.setState({
+                selectedOccupation: occupationValue.occupation,                                       
+                selectedRatingID: ratingValue.ratingid,
+                selectedFactor : ratingValue.factor
+            }, () => {
+                this.calculatePremium();
+            });
+    }
+
+    handleDateofBirthChange = (event: any) => {
+        let dobValue = new Date(event.target.value);
+        let currentDate = new Date();
+        let ageValue = currentDate.getFullYear() - dobValue.getFullYear();
+        if (ageValue > maxAge) {
+            ageValue = 0;
+        }
+        this.setState({
+            calculatedAge : ageValue
+        });
+    }
+
+    handleSumInsuredChange = (event: any) => {
+        let sumInsuredValue = event.target.value;
+        this.setState({
+            selectedSumInsured : sumInsuredValue
+        })
+    }
+
+    calculateMinDate() : string {
+        var date = new Date(); 
+        date.setFullYear(date.getFullYear() - maxAge);
+        console.log(Helpers.DateTimeHelpers.formatDate(date));
+        return Helpers.DateTimeHelpers.formatDate(date);
+    }
+
+    calculatePremium()
+    {
+        if(this.state.selectedName.length <=0 )        
+        {
+            toast.error("Name is not provided");
+        }
+        else if(this.state.calculatedAge <=0 )        
+        {
+            toast.error("Invalid Date of Birth");
+        }
+        else if(this.state.selectedOccupation.length <= 0)
+        {
+            toast.error("Occupation is not selected");
+        }
+        else if(this.state.selectedSumInsured <= 0)
+        {
+            toast.error("Sum Insured is not provided");
+        }
+        else {
+            let premiumValue : number = (this.state.selectedSumInsured * this.state.selectedFactor * this.state.calculatedAge)/1234;
+            this.setState({
+                calculatedMonthlyPremium : Number(premiumValue.toFixed(2))
+            });
+        }
+        this.calculateMinDate();
     }
 
 
     render() {
         return (
             <div>
+                <ToastContainer
+                        position="top-right"
+                        autoClose={5000}
+                        style={{ marginTop: "10px" }}
+                />
                 <Card style={{ marginTop: "50px" }}>
                     <CardHeader>
                         <h3>Premium Calculator</h3>
@@ -61,8 +144,20 @@ class PremiumCalculator extends React.Component<PremiumCalculatorProps, PremiumC
                             <Col xs="12" sm="12" md="2" lg="2">
                                 <Label htmlFor="select">Name</Label>
                             </Col>
-                            <Col xs="12" sm="12" md="6" lg="6">
-                                <input type="text"></input>
+                            <Col xs="12" sm="12" md="4" lg="4">
+                                <Input type="text" placeholder='Name' required
+                                    onChange={(event: any) => {
+                                        this.setState({
+                                            selectedName: event.target.value        
+                                        });
+                                    }}
+                                />
+                            </Col>
+                            <Col xs="12" sm="12" md="2" lg="2"/>
+                            <Col xs="12" sm="12" md="4" lg="4">
+                                <span>{this.state.calculatedMonthlyPremium > 0 ?
+                                    <h4>Monthy Premium : {this.state.calculatedMonthlyPremium}</h4>
+                                : null }</span>
                             </Col>
                         </Row>        
                         <Row>
@@ -70,39 +165,47 @@ class PremiumCalculator extends React.Component<PremiumCalculatorProps, PremiumC
                                 <Label htmlFor="select">Age</Label>
                             </Col>
                             <Col xs="12" sm="12" md="4" lg="4">
-                                <input type="number" disabled></input>
+                            <Input type="text" disabled value={this.state.calculatedAge}/>
                             </Col>
-                        </Row>        
+                        </Row>
                         <Row>
                             <Col xs="12" sm="12" md="2" lg="2">
                                 <Label htmlFor="select">Date of Birth</Label>
                             </Col>
                             <Col xs="12" sm="12" md="4" lg="4">
-                                <input type="date"></input>
+                                <Input 
+                                    type="date" 
+                                    onChange={this.handleDateofBirthChange.bind(this)}  
+                                    min={this.calculateMinDate()}                                  
+                                    //min="1953-02-03"                                  
+                                />
                             </Col>
-                        </Row>    
+                        </Row>
                         <Row>
                             <Col xs="12" sm="12" md="2" lg="2">
                                 <Label htmlFor="select">Occupation</Label>
                             </Col>
                             <Col xs="12" sm="12" md="4" lg="4">
-                                <select onClick={()=> { this.handleOccupationChange.bind(this) }}>{
-                                        this.state.occupations.map(item =>
+                                <Input type="select" placeholder='Select'
+                                    onChange={this.handleOccupationChange.bind(this)}
+                                >
+                                    {this.state.occupations.map(item =>
                                         <option key={item.occupation}>{item.occupation}</option>)
-                                }</select>;
+                                    }
+                                </Input>
                             </Col>
-                        </Row>    
+                        </Row>
                         <Row>
                             <Col xs="12" sm="12" md="2" lg="2">
                                 <Label htmlFor="select">Sum Insured</Label>
                             </Col>
                             <Col xs="12" sm="12" md="4" lg="4">
-                                <input type="number" disabled></input>
+                                <Input type="number" onChange={this.handleSumInsuredChange.bind(this)}/>
                             </Col>
-                        </Row>        
+                        </Row>    
                     </CardBody>
                     <CardFooter>
-                        <Button color="primary">Calculate</Button>
+                        <Button color="primary" onClick={this.calculatePremium.bind(this)}>Calculate</Button>
                     </CardFooter>
                 </Card>
             </div>
