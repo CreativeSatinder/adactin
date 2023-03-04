155989906c90dea650a221adf8a2d8c66d23be62
Satinder Singh <126883284+CreativeSatinder@users.noreply.github.com>
2023-03-04 15:44:01 -0500

    Add files via upload

---
+++ ClientApp/src/helpers/DateTimeHelpers.tsx
@@ -0,0 +1,17 @@
+export class DateTimeHelpers {
+    constructor() { }
+
+    public static formatDate = (date:Date) => {
+        var d = new Date(date),
+            month = '' + (d.getMonth() + 1),
+            day = '' + d.getDate(),
+            year = d.getFullYear();
+
+        if (month.length < 2)
+            month = '0' + month;
+        if (day.length < 2)
+            day = '0' + day;
+
+        return [year, month, day].join('-');
+    }
+}
